b56c595b8f130e9c1cc2141926e0939e0e604591
nhoxzoner <64214215+nhoxzoner@users.noreply.github.com>
2025-01-16 18:15:09 +0700

    Update productsApi.js

--- src/slices/productsApi.js
+++ src/slices/productsApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://backend-mauve-tau.vercel.app/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://web-production-71e6.up.railway.app/" }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => `products`,
